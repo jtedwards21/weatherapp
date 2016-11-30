960e6e518f0cf57900904574e9007654e1e91212
James Ahlm <14782828428@qq.com>
2016-11-30 15:12:13 +0800

    Changed Structure with cDM

--- public/javascripts/components/start.jsx
+++ public/javascripts/components/start.jsx
@@ -3,22 +3,32 @@ import Weather from "./weather";
 import axios from "axios";
 import ReactDOM from "react-dom";
 
-var onChange = function(data){
-  ReactDOM.render(<Weather data={data}/>, document.getElementById("content"));
-}
 
 export default class Start extends React.Component {
-  getWeather(city){
+　　constructor(props) {
+    super(props);
+
+    this.state = {
+      city: "",
+      data: []
+    };
+
+    this.updated = false;
+
+  }
+  componentDidMount(){
+　　　　var city = this.props.city;
     var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=92ce1e2eee7c91cb43470cada0b7c4d8";
     axios.get(url)
-    .then(data => onChange(data));
+    .then(data => {this.setState({data:data});
+       });
+  }
+  updateCity() {
+    console.log('dog');
   }
   render() {
-　　　　this.getWeather(this.props.city)
     return (
-      
-      <div>
-      </div>
+      <Weather data={this.state.data} oC={this.updateCity.bind(this)} />
     );
   }
 }
--- public/javascripts/components/weather.jsx
+++ public/javascripts/components/weather.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Start from "./start";
 
-
-var onChange = function(city, data){
-  if(this.props.updated == false){
-	var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=92ce1e2eee7c91cb43470cada0b7c4d8";
-        axios.get(url)
-        .then(data => ReactDOM.render(<Weather data={data} city={city} updated={true} />, document.getElementById("content")););
-  }
-}
 
 export default class Weather extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      
     };
 
-    this.updated = false;
   }
   
   getMainWeather(){
-　　　　if(this.props.data != undefined){
-    var d = this.props.data;
-    var main = d.weather[0];
-    var mainWeather = main.main;}
+　　　　if(this.props.data.length == 0){
+    　　var d = this.props.data;
+    　　var main = d.weather[0];
+    　　return mainWeather = main.main;
+    } else {
+    　　return "--";
+    }
+  }  
+  getBackground(){
+    if(this.props.data.length == 0){
+      var d = this.props.data;
+      var main = d.weather[0];
+      return mainWeather = main.main;
+    } else{
+      return "blue";
+    }
   }
   getTemp(){
-    if(this.props.data != undefined){
-    var d = this.props.data;
-    var base = d.base;
-    var temperature = base.main.temp;}
-  }
-  updateCity(){
-	//TO DO
+    if(this.props.data.length == 0){
+      var d = this.props.data;
+      var base = d.base;
+      return temperature = base.main.temp;
+    } else {
+      return "--";
+    }
   }
   render() {
     return (
@@ -42,13 +45,13 @@ export default class Weather extends React.Component {
         <div className="weather-box">
           <div className="title">Your Local Weather</div>
 	  <div className="info-box">
-	    <div>{(this.) ? this.state.mainWeather : "--"}</div>
-	    <div>{(this.getTemp.bind(this)) ? this.state.temperature : "--"}</div>
+	    <div>{this.getMainWeather.bind(this)}</div>
+	    <div>{this.getTemp.bind(this)}</div>
 	  </div>
           <input className="city-input" placeholder="Enter your city..." />
-	  <div className="go-btn" onClick={this.updateCity.bind(this)}>Go</div>
+	  <div className="go-btn" onClick={this.props.oC}>Go</div>
         </div>
-	<div id="background" className={(this.props.updated) ? this.getTemp.bind(this) : "blue" }></div>
+	<div id="background" className={this.getBackground.bind(this)}></div>
       </div>
     );
   }
