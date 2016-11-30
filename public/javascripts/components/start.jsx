e04fd7cadb1764efd24e4e243fe016c27d48a140
James Ahlm <14782828428@qq.com>
2016-11-30 11:29:07 +0800

    ...

--- public/javascripts/components/start.jsx
+++ public/javascripts/components/start.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Weather from "./weather";
+import axios from "axios";
+import ReactDOM from "react-dom";
+
+var onChange = function(data){
+  ReactDOM.render(<Weather data={data}/>, document.getElementById("content"));
+}
 
 export default class Start extends React.Component {
   getWeather(city){
     var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=92ce1e2eee7c91cb43470cada0b7c4d8";
     axios.get(url)
-    .then(data => this.processWeatherData(data));
+    .then(data => onChange(data));
   }
   render() {
+　　　　this.getWeather(this.props.city)
     return (
       
       <div>
-        <Weather data={this.getWeather(this.props.city)}/>
       </div>
     );
   }
--- public/javascripts/components/weather.jsx
+++ public/javascripts/components/weather.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Start from "./start";
 
-var onChange = function(city){
-  ReactDOM.render(<Start city={city}/>, document.getElementById("content"));
+
+var onChange = function(city, data){
+  if(this.props.updated == false){
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=92ce1e2eee7c91cb43470cada0b7c4d8";
+        axios.get(url)
+        .then(data => ReactDOM.render(<Weather data={data} city={city} updated={true} />, document.getElementById("content")););
+  }
 }
 
 export default class Weather extends React.Component {
@@ -12,16 +17,21 @@ export default class Weather extends React.Component {
 
     this.state = {
     };
+
+    this.updated = false;
   }
+  
   getMainWeather(){
+　　　　if(this.props.data != undefined){
     var d = this.props.data;
     var main = d.weather[0];
-    var mainWeather = main.main;
+    var mainWeather = main.main;}
   }
   getTemp(){
+    if(this.props.data != undefined){
     var d = this.props.data;
     var base = d.base;
-    var temperature = base.main.temp;
+    var temperature = base.main.temp;}
   }
   updateCity(){
 	//TO DO
@@ -32,13 +42,13 @@ export default class Weather extends React.Component {
         <div className="weather-box">
           <div className="title">Your Local Weather</div>
 	  <div className="info-box">
-	    <div>{this.getMainWeather(this)}</div>
-	    <div>{this.getTemp.bind(this)}</div>
+	    <div>{(this.) ? this.state.mainWeather : "--"}</div>
+	    <div>{(this.getTemp.bind(this)) ? this.state.temperature : "--"}</div>
 	  </div>
           <input className="city-input" placeholder="Enter your city..." />
 	  <div className="go-btn" onClick={this.updateCity.bind(this)}>Go</div>
         </div>
-	<div id="background" className={this.getMainWeather.bind(this)}></div>
+	<div id="background" className={(this.props.updated) ? this.getTemp.bind(this) : "blue" }></div>
       </div>
     );
   }
